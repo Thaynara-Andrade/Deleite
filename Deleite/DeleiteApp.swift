86117c3e52622a07cc994a089a07be9ba9596f56
Thaynara-Andrade <thaynara.silva.andrade07@aluno.ifce.edu.br>
2023-09-11 10:10:39 -0300

    Initial Commit

---
+++ Deleite/DeleiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeleiteApp.swift
+//  Deleite
+//
+//  Created by Thaynara da Silva Andrade on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct DeleiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
