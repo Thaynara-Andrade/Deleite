5cd7f9899f2cb274d0d12f3a95ef9077cb2eeb41
maahdisousa <mmdisousa@gmail.com>
2023-09-11 11:37:01 -0300

    TutorialView

---
+++ Deleite/TutorialView.swift
@@ -0,0 +1,20 @@
+//
+//  TutorialView.swift
+//  Deleite
+//
+//  Created by Marília de Sousa on 11/09/23.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TutorialView_Previews: PreviewProvider {
+    static var previews: some View {
+        TutorialView()
+    }
+}
