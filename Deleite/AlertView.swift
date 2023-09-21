868405adb0b4a323c6efc4e8bc770155b7078026
Thaynara-Andrade <thaynara.silva.andrade07@aluno.ifce.edu.br>
2023-09-21 08:49:28 -0300

    :lipistick: assets

---
+++ Deleite/AlertView.swift
@@ -0,0 +1,14 @@
+//
+//  AlertView.swift
+//  Deleite
+//
+//  Created by Thaynara da Silva Andrade on 19/09/23.
+//
+
+import SwiftUI
+
+struct AlertView: View {
+    
+    var body: some View {
+    }
+}
