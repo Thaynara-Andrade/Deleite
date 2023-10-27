7aea7ef06e1c75680198b7f179d3eabc76fe301c
Raina Rodrigues <rainarrodrigues@gmail.com>
2023-10-27 10:34:36 -0300

    create top navigation

--- Deleite/V2/View/ContainerOptionView.swift
+++ Deleite/V2/View/ContainerOptionView.swift
@@ -12,21 +12,44 @@ struct ContainerOptionView: View {
     
     @State var isShowTesteView: Bool = false
     
+    
+    
     var body: some View {
         NavigationView{
             ZStack{
                 Color("Text-Color")
                     .ignoresSafeArea(.all)
-                VStack{
+                
+                ScrollView{
+                    VStack{
+                        HStack(alignment: .center, spacing: 0) {
+                            Text("De")
+                                .foregroundColor(Color(red: 0.97, green: 0.87, blue: 0.51))
+                            Text("leite")
+                                .foregroundColor(Color.white)
+
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .font(
+                        Font.custom("Fredoka", size: 24)
+                        .weight(.semibold))
+                        .multilineTextAlignment(.center)
+                    }
+                    .border(Color.yellow)
+                    .padding(.bottom, 550)
+                    .frame(width: 390, alignment: .topLeading)
+                    .shadow(color: .black.opacity(0.3), radius: 0, x: 0, y: 0.5)
+
+                    
                     HStack(alignment: .center, spacing: 10) {
                         NavigationLink(destination: PostloginView()) {
                             Text("Já tenho fraco de vidro")
-                            .font(
-                            Font.custom("SF Pro Text", size: 17)
-                            .weight(.semibold)
-                            )
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(Color("Text-Color"))
+                                .font(
+                                    Font.custom("SF Pro Text", size: 17)
+                                        .weight(.semibold)
+                                )
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Color("Text-Color"))
                         }
                     }
                     .padding(.horizontal, 20)
@@ -38,12 +61,12 @@ struct ContainerOptionView: View {
                     HStack(alignment: .center, spacing: 10) {
                         NavigationLink(destination: PostloginView()) {
                             Text("Não tenho frasco")
-                            .font(
-                            Font.custom("SF Pro Text", size: 17)
-                            .weight(.semibold)
-                            )
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(Color("Text-Color"))
+                                .font(
+                                    Font.custom("SF Pro Text", size: 17)
+                                        .weight(.semibold)
+                                )
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Color("Text-Color"))
                         }
                         
                     }
@@ -53,10 +76,11 @@ struct ContainerOptionView: View {
                     .background(Color(red: 0.95, green: 0.87, blue: 0.62))
                     .cornerRadius(15)
                 }
+                .padding(.top, 0)
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
                 
             }
-            .padding(.bottom, -307)
-        }
+        }.navigationViewStyle(.stack) 
     }
 }
 #Preview {
