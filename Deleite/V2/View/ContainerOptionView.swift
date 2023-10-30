c8f9af5468dca3d8e29ef0bb8fa5bac27dca1f42
Raina Rodrigues <rainarrodrigues@gmail.com>
2023-10-30 10:16:04 -0300

    Adjusting the spacing between components

--- Deleite/V2/View/ContainerOptionView.swift
+++ Deleite/V2/View/ContainerOptionView.swift
@@ -22,7 +22,7 @@ struct ContainerOptionView: View {
                     .ignoresSafeArea(.all)
                 
                 ScrollView{
-                    VStack{
+                    VStack(alignment: .leading) {
                         HStack(alignment: .center, spacing: 0) {
                             Text("De")
                                 .foregroundColor(Color(red: 0.97, green: 0.87, blue: 0.51))
@@ -30,15 +30,17 @@ struct ContainerOptionView: View {
                                 .foregroundColor(Color.white)
 
                         }
-                        .frame(maxWidth: .infinity, alignment: .leading)
                         .font(
                         Font.custom("Fredoka", size: 24)
-                        .weight(.semibold))
+                        .weight(.semibold)
+                        )
                         .multilineTextAlignment(.center)
+                        .foregroundColor(Color(red: 0.97, green: 0.87, blue: 0.51))
+                        .frame(width: 82, height: 27, alignment: .top)
                     }
-                    .padding(.bottom, 100)
                     .frame(width: 390, alignment: .topLeading)
                     .shadow(color: .black.opacity(0.3), radius: 0, x: 0, y: 0.5)
+                    .padding(.leading, 30)
 
                     HStack{
                         Image(yellowBackground)
@@ -53,11 +55,11 @@ struct ContainerOptionView: View {
                                     .opacity(0.9)
                                     .offset(y: -9)
                             }
-                    }
+                    }.padding(.top, 40)
                     
                     VStack{
                         StepByStepView()
-                    }
+                    }.padding(.top, 35)
                     
                     NavigationLink(destination: PostloginView()) {
                         HStack(alignment: .center, spacing: 10) {
@@ -74,7 +76,7 @@ struct ContainerOptionView: View {
                         .frame(width: 326, alignment: .center)
                         .background(Color(red: 0.95, green: 0.87, blue: 0.62))
                         .cornerRadius(15)
-                    }
+                    }.padding(.top, 65)
                     
                     NavigationLink(destination: PostloginView()) {
                         HStack(alignment: .center, spacing: 10) {
