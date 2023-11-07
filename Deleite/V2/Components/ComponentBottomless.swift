308f802ae34811b89002eb87f4effd52df7252b7
Thaynara-Andrade <thaynara.silva.andrade07@aluno.ifce.edu.br>
2023-11-07 15:12:06 -0300

    :sparkles: adding tutorial screens and their components

---
+++ Deleite/V2/Components/ComponentBottomless.swift
@@ -0,0 +1,46 @@
+//
+//  ComponentBottomless.swift
+//  Deleite
+//
+//  Created by Thaynara da Silva Andrade on 07/11/23.
+//
+
+import SwiftUI
+
+struct ComponentBottomless: View {
+    let Buttomless: String
+    @State private var isPostLoginViewActive = false
+    
+    init(Buttomless: String) {
+        self.Buttomless = Buttomless
+    }
+    var body: some View {
+        
+        
+        Button(action: {
+            isPostLoginViewActive = true
+        }) {
+            HStack(alignment: .center, spacing: 10) {
+                Text(Buttomless)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color("Text-Color"))
+                
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 13)
+            .frame(width: 326, alignment: .center)
+            .cornerRadius(15)
+            .overlay(
+                RoundedRectangle(cornerRadius: 15)
+                    .inset(by: 1.5)
+                    .stroke(Color("Text-Color")))
+        } .fullScreenCover(isPresented: $isPostLoginViewActive) {
+            PostloginView()
+        }
+    }
+}
+
+#Preview {
+    ComponentBottomless(Buttomless: "Ir para agendamentos")
+}
+
--- Deleite/V2/View/ResumeView.swift
+++ Deleite/V2/View/ResumeView.swift
@@ -8,75 +8,75 @@
 import SwiftUI
 
 struct ResumeView: View {
-    var body: some View {
-        NavigationView{
-            ZStack(alignment: .topLeading) {
-            
-            Color("Text-Color")
-                .edgesIgnoringSafeArea(.all)
-            ScrollView (.vertical){
-                VStack{
-                    Text("Confira abaixo as poucas etapas que você \ndeve seguir para realizar sua doação de \nleite humano:")
-                        .foregroundColor(.white)
-                } .padding(.leading, -25)
-                    Rectangle()
-                        .frame(width: 5, height: 350)
-                        .padding(.leading, -140)
-                        .padding(.top,60)
-                        .foregroundColor(Color("yellow-color"))
-                    VStack{
-                        ComponentscircleView(LoginImage: Image("Eyes"))
-
-                    } .padding(.leading,-172)
-                        .padding(.top,-390)
-                    VStack{
-                        ComponentscircleView(LoginImage: Image("mama"))
-                    } .padding(.leading, -172)
-                        .padding(.top,-290)
-                    VStack{
-                        ComponentscircleView(LoginImage: Image("Calendario"))
-                    } .padding(.leading,-172)
-                        .padding(.top,-190)
-                    VStack{
-                        ComponentscircleView(LoginImage: Image("car"))
-                    } .padding(.leading,-172)
-                        .padding(.top,-90)
-                VStack(alignment: .leading){
-                    Text("Ver tutoriais")
-                        .bold()
-                        .padding(.bottom,82)
-                        
-                        
-                    Text("Retirada do leite")
-                        .bold()
-                        .padding(.bottom,82)
-                    
-                    Text("Agendamento")
-                        .bold()
-                        .padding(.bottom,82)
-                    
-                    Text("Coleta realizada")
-                        .bold()
-                        .padding(.bottom,50)
-                    
-                } .padding(.top, -400)
-                
-                VStack (alignment: .center){
-                    ButtonView(Textbutton: "Ver tutoriais", Textbutton2: "Ir para agendamentos")
-                } .padding(.leading,30)
-                  .padding(.trailing,30)
-                  .padding(.top, -60)
-                
-                
-                }
-                
-            }.navigationTitle("Resumo")
-
-                
-            
-            
-        }
+    @State private var isPostLoginViewActive = false
     
+    var body: some View {
+    
+            ZStack(alignment: .leading) {
+               
+                ScrollView (.vertical){
+                    Group{
+                        VStack{
+                            Text("Confira abaixo as poucas etapas que você \ndeve seguir para realizar sua doação de \nleite humano:")
+                                
+                        } .padding(.leading, -25)
+                        Rectangle()
+                            .frame(width: 5, height: 350)
+                            .padding(.leading, -140)
+                            .padding(.top, 20)
+                            .foregroundColor(Color("yellow-color"))
+                        VStack {
+                            ComponentscircleView(
+                                LoginImage: Image("Eyes"))
+                            .padding(.top,-20)
+                        
+                            ComponentscircleView(
+                                LoginImage: Image("mama"))
+                            .padding(.top,30)
+                            
+                            ComponentscircleView(
+                                LoginImage: Image("Calendario"))
+                            .padding(.top,30)
+                            
+                            ComponentscircleView(
+                                LoginImage: Image("car"))
+                            .padding(.top,30)
+                            
+                        }.padding(.leading,-172)
+                            .padding(.top,-350)
+                        
+                        VStack(alignment: .leading){
+                            Text("Ver tutoriais")
+                                .bold()
+                                .padding(.bottom,82)
+                            
+                            Text("Retirada do leite")
+                                .bold()
+                                .padding(.bottom,82)
+                            
+                            Text("Agendamento")
+                                .bold()
+                                .padding(.bottom,82)
+                            
+                            Text("Coleta realizada")
+                                .bold()
+                                .padding(.bottom,50)
+                            
+                        } .padding(.top, -380)
+                        
+                        
+                        
+                        VStack (alignment: .center){
+                            ButtonView(Textbutton: "Ver tutoriais", Textbutton2: "Ir para agendamentos")
+                        } .padding(.leading,30)
+                            .padding(.trailing,30)
+                            .padding(.top, -60)
+                    }
+                    
+                }
+            } .navigationTitle("Resumo")
+            .navigationBarTitleDisplayMode(.large)
+        
     }
 }
 
