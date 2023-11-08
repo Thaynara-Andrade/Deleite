24c1577bd2ca2b536a092e3e7c6ab6b42bcbf1d4
maahdisousa <mmdisousa@gmail.com>
2023-11-08 10:11:07 -0300

    :sparkles: app access code validation logic

--- Deleite/DeleiteApp.swift
+++ Deleite/DeleiteApp.swift
@@ -23,7 +23,7 @@ struct DeleiteApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContainerOptionView()
+            NoLoginView()
                 .environmentObject(vm)
         }
     }
--- Deleite/V2/View/NoLoginView.swift
+++ Deleite/V2/View/NoLoginView.swift
@@ -11,59 +11,65 @@ struct NoLoginView: View {
     
     var body: some View {
        
+        NavigationView{
+            
             ZStack(alignment: .topLeading) {
                 Color("Text-Color")
                     .edgesIgnoringSafeArea(.all)
                 
-                VStack(alignment: .leading) {
-                    HStack {
-                        Image("Deleite")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 110)
-                            .padding(.leading, 20)
-                            .padding(.bottom, 5)
-                            .padding(.top)
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Image("Deleite")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 110)
+                                .padding(.leading, 20)
+                                .padding(.bottom, 5)
+                                .padding(.top)
+                            
+                            Image(systemName: "info.circle")
+                                .font(.system(size: 25))
+                                .foregroundColor(.white)
+                                .padding(.leading, 200)
+                        }
+//                        
+//                        Text("Seja bem vinda!")
+//                            .padding(.leading, 20)
+//                            .foregroundColor(.white)
                         
-                        Image(systemName: "info.circle")
-                            .font(.system(size: 25))
-                            .foregroundColor(.white)
-                            .padding(.leading, 200)
+                        VStack{
+                            ComponentNoLoginView(Logintitle: "Seja \nDoadora", Loginname: "Faça seu cadastro aqui", LoginImage: Image("peito1"))
+                                .padding(.bottom, 30)
+                            
+                            Text("Já tem cadastro? Digite seu código aqui.")
+                                .padding(.leading, -100)
+                                .padding(.leading, 60)
+                                .foregroundColor(.white)
+                            
+                            ComponentTextField()
+                            
+                            Text("Agendamentos")
+                                .font(.system(size: 25))
+                                .fontWeight(.heavy)
+                                .foregroundColor(.white)
+                                .padding(.leading, -160)
+                                .padding(.top)
+                                .padding(.bottom, 3)
+                            
+                            VStack (alignment: .center){
+                                ComponentEmpytState(LoginImage:
+                                                        Image(systemName: "calendar.badge.plus"), Loginname: "Ainda não há agendamentos")
+                                .frame(height: 180)
+                            }
+                        }
+                        .padding()
                     }
-                    
-                    Text("Seja bem vinda!")
-                        .padding(.leading, 20)
-                        .foregroundColor(.white)
-                    
-                    VStack{
-                        ComponentNoLoginView(Logintitle: "Seja \nDoadora", Loginname: "Faça seu cadastro aqui", LoginImage: Image("peito1"))
-                            .padding(.bottom, 30)
-                        
-                        Text("Caso já possua senha, digite abaixo:")
-                            .padding(.leading, -100)
-                            .padding(.leading, 20)
-                            .foregroundColor(.white)
-                        
-                        ComponentTextFiel()
-                        
-                        
-                        Text("Agendamentos")
-                            .font(.system(size: 25))
-                            .fontWeight(.heavy)
-                            .foregroundColor(.white)
-                            .padding(.leading, -160)
-                            .padding(.top)
-                            .padding(.bottom, 3)
-                        
-                        VStack (alignment: .center){
-                        ComponentEmpytState(LoginImage:
-                                                Image(systemName: "calendar.badge.plus"), Loginname: "Ainda não há agendamentos")
-                        .frame(height: 180)
-                    }
-                    }
-                    .padding()
+                }
+                
             }
             
+            .ignoresSafeArea(.keyboard, edges: .bottom)
         }
     }
 }
