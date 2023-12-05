05a00b404d44cc3d9dfc26c2d8f5377986edffd0
Thaynara-Andrade <thaynara.silva.andrade07@aluno.ifce.edu.br>
2023-12-05 16:17:38 -0300

    :lipstick: postloginview card add

--- Deleite/V2/View/PostloginView.swift
+++ Deleite/V2/View/PostloginView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct PostloginView: View {
+    
     @State private var showAlert = false
     @State var openRegistrationSheet = false
     
@@ -15,38 +16,19 @@ struct PostloginView: View {
         
         NavigationView{
             ScrollView{
-                ZStack(alignment: .topLeading) {
-                    
-                    Button(action: {
-                        self.showAlert = true
-                    }) {
-                        Image(systemName: "questionmark.circle")
-                            .font(.system(size: 24))
-                            .foregroundColor(Color("icon-color"))
-                            .padding(.leading, 340)
-                            
-                    } .alert(isPresented: $showAlert) {
-                        Alert(
-                            title: Text("Ajuda"),
-                            message: Text("Para obter suporte, entre em \ncontato no botão abaixo"),
-                            primaryButton: .default(Text("Falar com Suporte"), action: {
-                                
-                                if let url = URL(string: "https://christianpaulo.com.br/deleite/") {
-                                    UIApplication.shared.open(url)
-                                }
-                            }),
-                            secondaryButton: .cancel(Text("Cancelar"))
-                        )
-                    }
+                ZStack{
                     
                     VStack(alignment: .leading) {
-                        
-                        Image("Deleite")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100)
-                            .padding(.top)
+                        HStack(spacing: 220){
                             
+                            Image("Deleite")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100)
+                                .padding(.top)
+                            
+                            ComponentAlert()
+                        }
                         
                         Text("Seja bem vinda!")
                         
@@ -55,38 +37,28 @@ struct PostloginView: View {
                             
                             Text("MEAC")
                                 .fontWeight(.bold)
-                                
-                            
-                            
                             
                         }
                         
-                        ZStack {
-                            ComponentDonationView(Logintitle: "Fazer \ndoação", Loginname: "Agende o recebimento \ndo seu potinho ou a melhor \ndata para fazer a coleta do leite", LoginImage: Image("Doar"))
-                                
-                        }
-                            HStack (spacing: 73) {
-                                Text("Agendamentos")
-                                    .font(.system(size: 25))
-                                    .fontWeight(.bold)
-                                    .padding(.leading)
-                                    .padding(.top)
-                                
-                                
-                                Text("Ver todos")
-                                    .padding(.trailing)
+                        ComponentDonationView(Logintitle: "Fazer \ndoação", Loginname: "Agende aqui o melhor \ndia para sua doação", LoginImage: Image("Doar"))
+                        
+                        
+                        HStack (spacing: 100) {
+                            Text(" Agendamentos")
+                                .font(.system(size: 25))
+                                .fontWeight(.bold)
+                                .padding(.top)
+                            
+                            NavigationLink(destination: ComponentTextView()) {
+                                Text("Histórico")
                                     .padding(.top)
                                     .foregroundColor(.gray)
-                                
                             }
-                            
-                            VStack (alignment: .center){
-                                ModelScheduling()
-                                
-                            }
-                        
-                        
-                    } .padding(.leading)
+                        }
+                    }
+                    
+                    ModelScheduling()
+                        .padding(.bottom,-230)
                 }
             }
         }
