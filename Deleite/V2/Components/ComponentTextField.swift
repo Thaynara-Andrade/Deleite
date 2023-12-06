3b3935840c61cb648100d24e70dc2c1363ba058a
maahdisousa <mmdisousa@gmail.com>
2023-12-06 11:40:40 -0300

    inserir codigo atualizado

--- Deleite/V2/Components/ComponentTextField.swift
+++ Deleite/V2/Components/ComponentTextField.swift
@@ -17,47 +17,47 @@ struct ComponentTextField: View {
         
         HStack{
             VStack{
-            TextField("Digite o código", text: $password)
-                .textFieldStyle(PlainTextFieldStyle())
-                .padding(.leading)
-                .focused($passwordIsFocused)
-                .background(
-                    Rectangle()
-                        .frame(width: 350, height: 55)
-                        .foregroundColor(Color.gray.opacity(0.2))
-                        .cornerRadius(15)
-                )
-                .padding()
-                .textFieldStyle(.roundedBorder)
-                .overlay(
-                    
-                    VStack {
-                        Button(action: {
-                            if (password == "001") {
-                                isLogged = true
-                            } else {
-                                showErrorMessage = true
-                            }
-                            passwordIsFocused = false
-                        }) {
-                            
-                            Text("Entrar")
-                                .fontWeight(.semibold)
-                                .frame(width: 280, height: 14)
-                                .padding()
-                                .background(Color("Text-Color"))
-                                .foregroundColor(Color.white)
-                                .cornerRadius(15)
-                                .padding(.top, 180)
+                TextField("Digite o código", text: $password)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding(.leading)
+                    .focused($passwordIsFocused)
+                    .background(
+                        Rectangle()
+                            .frame(width: 350, height: 55)
+                            .foregroundColor(Color.gray.opacity(0.2))
+                            .cornerRadius(15)
+                    )
+                    .padding()
+                    .textFieldStyle(.roundedBorder)
+                
+                VStack {
+                    Button(action: {
+                        if (password == "001") {
+                            isLogged = true
+                        } else {
+                            showErrorMessage = true
                         }
+                        passwordIsFocused = false
+                    }) {
+                        
+                        Text("Entrar")
+                            .fontWeight(.semibold)
+                            .frame(width: 280, height: 14)
+                            .padding()
+                            .background(Color("Text-Color"))
+                            .foregroundColor(Color.white)
+                            .cornerRadius(15)
+                            .padding(.top, 25)
                     }
-                )}
-    }
+                }
+            }
+        }
         
         .padding(.top, 180)
         .alert(isPresented: $showErrorMessage) {
             Alert(title: Text("Erro"), message: Text("Por favor, tente novamente."), dismissButton: .default(Text("Ok")))
         }
+        
         .fullScreenCover(isPresented: $isLogged) {
             PostloginView()
         }
