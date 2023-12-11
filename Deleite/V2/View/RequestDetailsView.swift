4d2062ed66251c95f838d42695381dbb9242052c
Raina Rodrigues <rainarrodrigues@gmail.com>
2023-12-11 08:46:43 -0300

    trying to make a spreadsheet

---
+++ Deleite/V2/View/RequestDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  RequestDetailsView.swift
+//  Deleite
+//
+//  Created by Raina on 10/12/23.
+//
+
+import SwiftUI
+
+struct RequestDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RequestDetailsView()
+}
